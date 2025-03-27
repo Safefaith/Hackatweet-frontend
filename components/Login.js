1e58c581d70e9dfb0478866c328d1efa4969009a
Steven <steven.fabregue@gmail.com>
2025-03-27 14:26:51 +0100

    Frontend, composent Home + page home

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,26 @@
 import styles from "../styles/Login.module.css";
 import Image from "next/image";
+import { useState } from "react";
+import ModalSignUp from "./Signup";
+import Link from "next/link";
 
 function Login() {
   return (
     <div>
+      {/* <ModalSignUp isOpen={isOpen} handleClose={handleClose}>
+        <header>
+          <h1>Hello world</h1>
+        </header>
+      </ModalSignUp> */}
       <main className={styles.main}>
         <div className={styles.leftMain}>
           <Image src="/logo.png" alt="Logo" width={300} height={300} />
         </div>
         <div className={styles.rightMain}>
           <Image src="/logo.png" alt="Logo" width={80} height={80} />
+          <Link href="/home">
+            <button>Page Home</button>
+          </Link>
           <h1 className={styles.h1Main}>See what's happening</h1>
           <h2 className={styles.h2Main}>Join Hackatweet today.</h2>
           <button className={styles.signUpBtn}>Sign up</button>
