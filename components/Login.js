775bbe48ec831a93e5552f90efe17b734509d897
Steven <steven.fabregue@gmail.com>
2025-03-27 11:40:09 +0100

    Première page login, sans inputs ni modal

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,20 @@
 import styles from "../styles/Login.module.css";
+import Image from "next/image";
 
 function Login() {
   return (
     <div>
       <main className={styles.main}>
         <div className={styles.leftMain}>
-          <img src="."></img>
+          <Image src="/logo.png" alt="Logo" width={300} height={300} />
+        </div>
+        <div className={styles.rightMain}>
+          <Image src="/logo.png" alt="Logo" width={80} height={80} />
+          <h1 className={styles.h1Main}>See what's happening</h1>
+          <h2 className={styles.h2Main}>Join Hackatweet today.</h2>
+          <button className={styles.signUpBtn}>Sign up</button>
+          <h3 className={styles.h3Main}>Already have an account?</h3>
+          <button className={styles.signInBtn}>Sign in</button>
         </div>
       </main>
     </div>
